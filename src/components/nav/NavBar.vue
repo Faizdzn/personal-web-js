b5e9a0abfe59cfd8b0ddcb53d9bcce85c6887a79
Faizdzn <masterfrostgt@gmail.com>
2024-07-15 11:00:47 +0700

    e

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -3,7 +3,7 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-    <div class="md:mt-[48px] md:text-[20px] flex items-center justify-between max-md:fixed max-md:w-screen max-md:bg-dark max-md:border-b-2 max-md:py-5 max-md:px-4 max-md:border-white/15 z-10">
+    <div class="z-[10000] md:mt-[48px] md:text-[20px] flex items-center justify-between max-md:fixed max-md:w-screen max-md:bg-dark max-md:border-b-2 max-md:py-5 max-md:px-4 max-md:border-white/15 z-10">
       <div class="flex items-center gap-[20px]">
         <img src="@/assets/decal/poto_bg_white.png" class="max-md:hidden rounded-full" width="50">
         <span>faiz_.da</span>
