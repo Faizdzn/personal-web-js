2573c21661b5f55a5d70f6e19d1c38d421fbcf0e
Faizdzn <masterfrostgt@gmail.com>
2024-07-15 16:50:51 +0700

    e

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@ import FooterComp from '@/components/footer/FooterComp.vue'
 </script>
 
 <template>
+  <img class="absolute w-full z-0 max-md:top-11" src="@/assets/decal/rays.png">
   <main class="relative flex flex-col max-w-[950px] mx-auto md:px-4 overflow-clip">
-    <img class="mx-auto absolute z-0 max-md:top-11" src="@/assets/decal/rays.png">
     <NavBar />
     <view class="mt-[85px] flex flex-col gap-[25px] max-md:px-4">
       <RouterView />
